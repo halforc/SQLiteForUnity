a43997fb4f6507627c39c4582aa9c4edd0426ea7
halforc <570519154@qq.com>
2018-04-04 19:21:41 +0800

    SQLite helper and tests

---
+++ Scripts/LineTest.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class LineTest : MonoBehaviour {
+
+	private LineRenderer linerender;
+	//定义一个Vector3,用来存储鼠标点击的位置  
+	private Vector3 position;
+	//用来索引端点  
+	private int index = 0;
+	//端点数  
+	private int LengthOfLineRenderer = 0;
+	// Use this for initialization
+	void Start () {
+		linerender = gameObject.GetComponent<LineRenderer>();
+		//linerender.material = new Material(Shader.Find("Particles/Additive"));
+		//linerender.SetColors(Color.red, Color.red);
+		linerender.SetWidth(0.002f, 0.002f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//鼠标左击  
+		if(Input.GetMouseButtonDown(0)) {
+			//将鼠标点击的屏幕坐标转换为世界坐标，然后存储到position中  
+			position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f));
+			//端点数+1  
+			LengthOfLineRenderer++;
+			//设置线段的端点数  
+			linerender.SetVertexCount(LengthOfLineRenderer);
+
+		}
+		//连续绘制线段  
+		while(index < LengthOfLineRenderer) {
+			//两点确定一条直线，所以我们依次绘制点就可以形成线段了  
+			linerender.SetPosition(index, position);
+			index++;
+		}   
+	}
+
+	void OnGUI() {
+		GUILayout.Label("当前鼠标X轴位置：" + Input.mousePosition.x);
+		GUILayout.Label("当前鼠标Y轴位置：" + Input.mousePosition.y);
+	} 
+}
---
+++ Scripts/testSQLite.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mono.Data.Sqlite;
+using System;
+
+public class testSQLite : MonoBehaviour {
+
+	SqliteConnection sqlConnect;
+
+	// Use this for initialization
+	void Start () {
+		try {
+			sqlConnect = new SqliteConnection("data source=sqliteTest.db");
+			sqlConnect.Open();
+		}
+		catch(Exception e) {
+			Debug.Log(e.Message);
+		}
+		SqliteCommand dbCommand = sqlConnect.CreateCommand();
+		string strCommand = "CREATE TABLE IF NOT EXISTS testInfo(id integer primary key,name TEXT,age INTEGER)";
+		dbCommand.CommandText = strCommand;
+		dbCommand.ExecuteReader();
+
+		SqliteCommand dbCommand1 = sqlConnect.CreateCommand();
+		strCommand = "insert into testInfo values(1,'Tom',22)";
+		dbCommand1.CommandText = strCommand;
+		dbCommand1.ExecuteReader();
+
+		SqliteCommand dbCommand2 = sqlConnect.CreateCommand();
+		strCommand = "select * from testInfo";
+		dbCommand2.CommandText = strCommand;
+		SqliteDataReader reader = dbCommand2.ExecuteReader();
+		while(reader.Read()) {
+			Debug.Log(reader.GetInt32(reader.GetOrdinal("id")));
+			Debug.Log(reader.GetString(reader.GetOrdinal("name")));
+			Debug.Log(reader.GetInt32(reader.GetOrdinal("age")));
+		}
+		sqlConnect.Close();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
